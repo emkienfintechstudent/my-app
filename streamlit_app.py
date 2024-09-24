be427912e16c1b3afc0c283602e4e866e17bffc8
Trần Trung Kiên <147416326+emkienfintechstudent@users.noreply.github.com>
2024-09-24 16:23:04 +0000

    tesst

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title('🎈 App Name')
 
-st.image("/workspaces/my-app/z5864160814297_b1b373bfdafbdc1fc151662d22b692e5.jpg", caption="a Phương")
+st.image("z5864160814297_b1b373bfdafbdc1fc151662d22b692e5.jpg", caption="a Phương")
